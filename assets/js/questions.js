78ca022917126df39dd8e5a0813d39492a5fec73
Giada Sponzilli <sponz@MacBook-Pro-di-Giada.local>
2024-01-02 13:17:23 +0100

    added array of objects for the questions

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -4,6 +4,34 @@
   // Set of answers
   // Which answer is correct
 
+let questions = [
+    {
+        question1: "Commonly used data types DO NOT include:",
+        answers1: ["1. strings", "2. booleans", "3. alerts", "4. numbers"],
+        correctAnswer1: 2,
+    },
+    {
+        question2: "The condition in an if / else statement is enclosed within ____ .",
+        answers2: ["1. quotes", "2. curly brackets", "3. parentheses", "4. square brackets"],
+        correctAnswer2: 2,
+    },
+    {
+        question3: "Arrays in JavaScript can be used to store ____ .",
+        answers3: ["1. numbers and strings", "2. other arrays", "3. booleans", "4. all of the above"],
+        correctAnswer3: 3,
+    },
+    {
+        question4: "String values must be enclosed within ____ when being assigned to variables",
+        answers4: ["1. commas", "2. curly brackets", "3. quotes", "4. parentheses"],
+        correctAnswer4: 2,
+    },
+    {
+        question5: "A very useful tool used during development and the bugging for printing content to the bugger is:",
+        answers5: ["1. JavaScript", "2. terminal / bash", "3. for loops", "4. console log"],
+        correctAnswer5: 3,
+    },
+]
+
 // Landing page:
   // Explanation of the quiz
   // Start button
